4523cd49ae8cfe3bd8c6f231381aacc5702936e9
CarolineVeleda <carollimaveleda@yahoo.com.br>
2018-04-17 16:18:18 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -7,16 +7,20 @@
 //This is the "Offline page" service worker
 
 //Install stage sets up the offline page in the cahche and opens a new cache
-self.addEventListener('install', function(event) {
-  var offlinePage = new Request('offline.html');
-  event.waitUntil(
-  fetch(offlinePage).then(function(response) {
-    return caches.open('pwabuilder-offline').then(function(cache) {
-      console.log('[PWA Builder] Cached offline page during Install'+ response.url);
-      return cache.put(offlinePage, response);
-    });
-  }));
+self.addEventListener('install', function (event) {
+    var indexPage = new Request('index.html');
+    event.waitUntil(
+        fetch(indexPage).then(function (response) {
+            caches.open('pwabuilder-offline').then(function (cache) {
+                console.log('[PWA Builder] Cached index page during Install' + response.url);
+                return cache.addAll(['/GeekStore/', '/GeekStore/index.html', '/magnolia/cursos.html',
+                  '/GeekStore/sw.js','/GeekStore/sw.js',
+                ]);
+            });
+        })
+    );
 });
+
 
 //If any fetch fails, it will show the offline page.
 //Maybe this should be limited to HTML documents?
